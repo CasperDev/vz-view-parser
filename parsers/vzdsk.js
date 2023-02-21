bab86949b0adef30c39c7786e1dad599b3ccb3c3
CasperDev <casperdev@4ab.pl>
2023-02-21 14:55:16 +0100

    DSK improved view and compatibility

--- parsers/vzdsk.js
+++ parsers/vzdsk.js
@@ -1,7 +1,4 @@
 
-const HeaderVar1Start = [0x80,0x80,0x80,0x80,0x80,0x80,0x80, 0x00, 0xfe,0xe7,0x18,0xc3];
-const HeaderVar2Start = [0x80,0x80,0x80,0x80,0x80,0x80, 0x00, 0xfe,0xe7,0x18,0xc3];
-
 /**
  * Select the 'vz' extension with VZF0 header.
  */
@@ -27,55 +24,55 @@ registerFileType((fileExt, filePath, fileData) => {
  * For performance reasons the sequence of sector arrangement on track is:
  * 0, 11, 6, 1, 12, 7, 2, 13, 8, 3, 14, 9, 4, 15, 10, 5
  * NOTE: Disk image file doesn't have to follow this sequence.
+ * 
+ * The DOS uses Track 0, Sector 0 to Sector 14 as the directory.
+ * Track 0 Sector 15 is used to hold the track map of the disk with one bit 
+ * corresponding to a sector used. 
+ * 
+ * Each directory entry contains 16 bytes. Therefore 1 sector 
+ * can hold 8 entries and 1 diskette can have a maximum of 112 entries.
+ * 
+ */
+
+/********
+ * Directory entry structure
+ *
+ * 0	1 byte	File type char ('T','B',\01 (hidden?? BASIC))
+ * 1	1 byte	Delimitor char ':' (0x3a)
+ * 2	8 bytes	File name
+ * 10	1 byte	Start track
+ * 11	1 byte	Start sector
+ * 12	2 bytes	16-bit Start address
+ * 14	2 bytes	16-bit End address
+ * Total 16 bytes -------------------------
+
  */
 /*******
  * Disk sector structure:
- * 0  	25 bytes 	- Header with track and sector number
+ * 0  	25 bytes 	- Header with track and sector number (see NOTE below)
  * 25	128 bytes 	- sector data
  * 153	2 bytes		- 16bit CRC of sector data
  * Total 155 bytes  --------------------------
  */
+
 /*******
- * Header structure (variant no 1 as documented in VZ300 Technical Manual):
+ * Header structure (as documented in VZ300 Technical Manual):
  * 0	7 bytes		- GAP1 = 0x80,0x80,0x80,0x80,0x80,0x80,0x80
- * 7	1 byte		- GAP1 = 0x00
+ * 7	1 byte		- GAP1 End = 0x00
  * 8	4 bytes		- IDAM Start = 0xfe, 0xe7, 0x18, 0xc3
  * 12	1 byte		- Track number
  * 13	1 byte		- Sector number
  * 14	1 byte		- CRC of Track and Sector Number (simbly T+S)
  * 15	5 bytes		- GAP2 = 0x80,0x80,0x80,0x80,0x80
- * 20	1 byte		- GAP2 = 0x00
+ * 20	1 byte		- GAP2 End = 0x00
  * 21   4 bytes		- IDAM End = 0xc3, 0x18, 0xe7, 0xfe
  * Total 25 bytes	------------------------------------------------------
+ * 
+ * NOTE: Number of 0x80 bytes in GAP1 and GAP2 varies from 5 to 7.
+ *       This can be inconsistient even in the same disk image file.
  */
 
-/*******
- * Header structure (variant no 2):
- * 0	6 bytes		- GAP1 = 0x80,0x80,0x80,0x80,0x80,0x80
- * 6	1 byte		- GAP1 = 0x00
- * 7	4 bytes		- IDAM Start = 0xfe, 0xe7, 0x18, 0xc3
- * 11	1 byte		- Track number
- * 12	1 byte		- Sector number
- * 13	1 byte		- CRC of Track and Sector Number (simbly T+S)
- * 14	5 bytes		- GAP2 = 0x80,0x80,0x80,0x80,0x80
- * 19	1 byte		- GAP2 = 0x00
- * 20   4 bytes		- IDAM End = 0xc3, 0x18, 0xe7, 0xfe
- * Total 24 bytes	------------------------------------------------------
- */
 
-/*******
- * Header structure (variant no 3):
- * 0	5 bytes		- GAP1 = 0x80,0x80,0x80,0x80,0x80
- * 5	1 byte		- GAP1 = 0x00
- * 6	4 bytes		- IDAM Start = 0xfe, 0xe7, 0x18, 0xc3
- * 10	1 byte		- Track number
- * 11	1 byte		- Sector number
- * 12	1 byte		- CRC of Track and Sector Number (simbly T+S)
- * 13	5 bytes		- GAP2 = 0x80,0x80,0x80,0x80,0x80
- * 18	1 byte		- GAP2 = 0x00
- * 19   4 bytes		- IDAM End = 0xc3, 0x18, 0xe7, 0xfe
- * Total 23 bytes	------------------------------------------------------
- */
 
 function getHexByte(b) {
 	var c = b.toString(16).toUpperCase();
@@ -131,9 +128,15 @@ registerParser(() => {
     addStandardHeader();
 	// 40 tracks to read
 
-	for(let sec=0; sec < 40*16; sec++) {
+	for(let trk=0; trk < 40; trk++) {
+	
+		let currentTrk = trk;
+		readRowWithDetails("Track", () => {
 
+	for(let sec=0; sec < 16; sec++) {
 
+		readRowWithDetails("Sector", ()=>{
+		
 		read(27);
 		let startOffset = getOffset();
 		let headerData = getData();
@@ -142,6 +145,8 @@ registerParser(() => {
 			setOffset(startOffset);
 			read(header.headerLen);
 		}
+
+		currentTrk = header.trackNo;
 
 		addRow("Header", 'T:'+header.trackNo+' S:'+header.secNo, 
 				'Header of Sector '+header.secNo+' on Track '+header.trackNo);
@@ -194,5 +199,23 @@ registerParser(() => {
 		read(2);
 		let secCrc = getHexValue().toUpperCase();
 		addRow('Sec CRC', '0x'+secCrc, 'CRC of Sector data');
+
+		return {
+			value: ''+header.secNo,
+			description: 'Sector '+header.secNo+' on Track '+header.trackNo
+		};
+	});
 	}
+	// some disk images have 15 or 16 bytes long (0x00) track separator 
+	if (!endOfFile()) {
+		read(16);
+		addMemDump();
+	}
+	return {
+		value: currentTrk,
+		description: 'Track '+currentTrk+' ('+trk+')'
+	};
+});
+}
+
 });
